85b193ab3878401b0357e850e1545ddbfbb4dcaf
pupu <investor@live.com>
2015-12-26 23:37:41 +0800

    update

---
+++ jsonrpc/client.go
@@ -0,0 +1,64 @@
+package jsonrpc
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+type Client struct {
+	Url string
+	id int
+}
+
+type Response struct {
+	Id int `json:"id"`
+	Result interface{} `json:"result"`
+	Error *ResponseError `json:"error"`
+}
+
+type ResponseError struct {
+	Code int `json:"code"`
+	Message string `json:"message"`
+	Data interface{} `json:"data"`
+}
+
+func NewClient(url string) *Client {
+	client := new(Client)
+	client.Url = url
+	return client
+}
+
+func (c *Client) Call(method string, params interface{}) (*Response, error) {
+	var payload = map[string]interface{}{
+		"jsonrpc": "2.0",
+		"method":  method,
+		"params":  params,
+		"id":      c.id,
+	}
+
+	c.id += 1
+	data, err := json.Marshal(payload)
+	if err != nil {
+		return nil, err
+	}
+	buf := bytes.NewBuffer(data)
+	resp, err := http.Post(c.Url, "application/json", buf)
+
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	decoder := json.NewDecoder(resp.Body)
+	var respPayload *Response
+	err = decoder.Decode(&respPayload)
+
+	if err != nil {
+		return nil, errors.New(fmt.Sprintf("Invalid response from server. Status: %s. %s", resp.Status, err.Error()))
+	}
+
+	return respPayload, nil
+}
